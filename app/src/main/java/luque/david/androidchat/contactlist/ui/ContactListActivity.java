bc7535226b31c791ed5b341d4632708606e94f5c
Davids89 <daviluqui@gmail.com>
2016-09-22 12:27:51 -0700

    Implementing the view

--- app/src/main/java/luque/david/androidchat/contactlist/ui/ContactListActivity.java
+++ app/src/main/java/luque/david/androidchat/contactlist/ui/ContactListActivity.java
@@ -1,15 +1,75 @@
 package luque.david.androidchat.contactlist.ui;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import luque.david.androidchat.R;
+import luque.david.androidchat.contactlist.ContactListPresenter;
+import luque.david.androidchat.entities.User;
 
-public class ContactListActivity extends AppCompatActivity {
+public class ContactListActivity extends AppCompatActivity implements ContactListView{
+
+    private ContactListPresenter presenter;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.recyclerViewContacts)
+    RecyclerView recyclerViewContacts;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        ButterKnife.bind(this);
+
+        presenter.onCreate();
+        toolbar.setTitle(presenter.getCurrentUserEmail());
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        presenter.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        presenter.onResume();
+        super.onResume();
+    }
+
+    @OnClick(R.id.fab)
+    public void addContact(){
+
+    }
+
+    @Override
+    public void onContact(User user) {
+
+    }
+
+    @Override
+    public void onContactChanged(User user) {
+
+    }
+
+    @Override
+    public void onContactRemoved(User user) {
+
     }
 }
