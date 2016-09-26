93380a13b6d6a2057eff7a19780c8d57baf54e26
Davids89 <daviluqui@gmail.com>
2016-09-26 13:59:42 -0700

    Loader completly prepared

--- app/src/main/java/luque/david/androidchat/contactlist/ui/adapters/ContactListAdapter.java
+++ app/src/main/java/luque/david/androidchat/contactlist/ui/adapters/ContactListAdapter.java
@@ -1,13 +1,17 @@
 package luque.david.androidchat.contactlist.ui.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import luque.david.androidchat.R;
 import luque.david.androidchat.entities.User;
 
@@ -18,12 +22,12 @@ import luque.david.androidchat.entities.User;
 public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder> {
 
     private List<User> contactList;
-    private ImageLoading imageLoading;
+    private ImageLoading imageLoader;
     private OnItemClickListener onItemClickListener;
 
-    public ContactListAdapter(List<User> contactList, ImageLoading imageLoading, OnItemClickListener onItemClickListener) {
+    public ContactListAdapter(List<User> contactList, ImageLoading imageLoader, OnItemClickListener onItemClickListener) {
         this.contactList = contactList;
-        this.imageLoading = imageLoading;
+        this.imageLoader = imageLoader;
         this.onItemClickListener = onItemClickListener;
     }
 
@@ -35,7 +39,19 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = contactList.get(position);
+        holder.setClickListener(user, onItemClickListener);
 
+        String email = user.getEmail();
+        boolean online = user.isOnline();
+        String status = online ? "online" : "offline";
+        int color = online ? Color.GREEN : Color.RED;
+
+        holder.txtUser.setText(email);
+        holder.txtStatus.setText(status);
+        holder.txtStatus.setTextColor(color);
+
+        imageLoader.load(holder.imgAvatar, "");
     }
 
     @Override
@@ -44,8 +60,36 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.imgAvatar)
+        CircleImageView imgAvatar;
+        @Bind(R.id.txtUser)
+        TextView txtUser;
+        @Bind(R.id.txtStatus)
+        TextView txtStatus;
+        private View view;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.view = view;
+        }
+
+        private void setClickListener(final User user, final OnItemClickListener listener){
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    listener.onItemClick(user);
+                }
+            });
+
+            view.setOnLongClickListener(new View.OnLongClickListener(){
+                @Override
+                public boolean onLongClick(View view) {
+                    listener.onItemLongClick(user);
+                    return false;
+                }
+            });
         }
     }
 }
--- app/src/main/java/luque/david/androidchat/contactlist/ui/adapters/ImageLoading.java
+++ app/src/main/java/luque/david/androidchat/contactlist/ui/adapters/ImageLoading.java
@@ -1,7 +1,12 @@
 package luque.david.androidchat.contactlist.ui.adapters;
 
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * Created by david on 22/9/16.
  */
 public interface ImageLoading {
+    void load(ImageView imgAvatar, String url);
 }
