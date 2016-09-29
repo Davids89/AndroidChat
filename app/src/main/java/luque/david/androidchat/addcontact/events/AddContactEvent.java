3ed3a7b1e9ffb73e633f1bb73bf7e7702bbfa5e9
Davids89 <daviluqui@gmail.com>
2016-09-29 11:51:22 -0700

    Finished add contact

--- app/src/main/java/luque/david/androidchat/addcontact/events/AddContactEvent.java
+++ app/src/main/java/luque/david/androidchat/addcontact/events/AddContactEvent.java
@@ -4,4 +4,13 @@ package luque.david.androidchat.addcontact.events;
  * Created by david on 27/9/16.
  */
 public class AddContactEvent {
+    boolean error = false;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
 }
