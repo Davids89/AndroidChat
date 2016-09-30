3ea14b55d531811828bc821a0c7505b581a7f9ef
Davids89 <daviluqui@gmail.com>
2016-09-30 16:33:06 -0700

    Preparing chat layout

---
+++ app/src/main/java/luque/david/androidchat/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package luque.david.androidchat.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import luque.david.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
