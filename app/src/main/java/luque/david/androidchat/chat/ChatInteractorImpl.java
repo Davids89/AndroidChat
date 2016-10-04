753ced87df11a9a41919bdf195b7cf0883ed602c
Davids89 <daviluqui@gmail.com>
2016-10-04 13:06:26 -0700

    Preparing the presenter

---
+++ app/src/main/java/luque/david/androidchat/chat/ChatInteractorImpl.java
@@ -0,0 +1,31 @@
+package luque.david.androidchat.chat;
+
+/**
+ * Created by david on 4/10/16.
+ */
+public class ChatInteractorImpl implements ChatInteractor {
+    @Override
+    public void sendMessage(String msg) {
+
+    }
+
+    @Override
+    public void setRecipient(String recipient) {
+
+    }
+
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+}
---
+++ app/src/main/java/luque/david/androidchat/chat/ChatSessionInteractorImpl.java
@@ -0,0 +1,11 @@
+package luque.david.androidchat.chat;
+
+/**
+ * Created by david on 4/10/16.
+ */
+public class ChatSessionInteractorImpl implements ChatSessionInteractor {
+    @Override
+    public void changeConnectionStatus(boolean online) {
+
+    }
+}
